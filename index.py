5d8a606c7646f0e2fc8b16c652d4bea608c0133a
Ketan.K.Sonwane <141003493+ketansonwane1@users.noreply.github.com>
2024-08-15 03:47:45 +0530

    Update and rename app.py to index.py

--- index.py
+++ index.py
@@ -1,12 +1,11 @@
-# app/app.py
-
+# index.py
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hi'
+def home():
+    return "Hello, World! This is served on port 5000."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
