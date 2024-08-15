1987145d50e16fe62f06df840d4069df59a2e97b
Ketan.K.Sonwane <141003493+ketansonwane1@users.noreply.github.com>
2024-08-15 15:01:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, send_from_directory
+
 app = Flask(__name__)
-Make sure disabling CSRF protection is safe here.
-Comment
+
+# Route for serving the static HTML file
 @app.route('/')
 def home():
     return send_from_directory('.', 'index.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
