15ee70de35fd8056ae947b13f242dc21704fc86f
Ketan.K.Sonwane <141003493+ketansonwane1@users.noreply.github.com>
2024-08-15 15:00:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, send_from_directory
-
 app = Flask(__name__)
-
+Make sure disabling CSRF protection is safe here.
+Comment
 @app.route('/')
 def home():
     return send_from_directory('.', 'index.html')
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
